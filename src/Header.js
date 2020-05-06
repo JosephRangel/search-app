f506d46eca434061ad9fea3ec15f64c2d1e4f6d3
Jose Rangel <ja.reyes@samsung.com>
2020-05-05 19:05:29 -0700

    change lighthouse img from internal img from iconfinder

--- src/Header.js
+++ src/Header.js
@@ -40,7 +40,7 @@ function Header() {
         <form class="w-full">
           <div class="w-full flex items-end border-b border-b-2 border-teal-500 py-3">
             <div class="text-gray-700 text-center w-15 mx-6">
-              <img src={window.location.origin + '/lighthouse.png'} alt="Smiley face" height="42" width="42"></img>
+              <img src="https://cdn1.iconfinder.com/data/icons/summer-7/512/travel_lighthouse-512.png" alt="Smiley face" height="42" width="42"></img>
             </div>
             <input id="search" class="xl:w-1/4 md:w-3/6 sm:w-3/6 ml-6 rounded p-2 text-gray-700" type="search" placeholder="Search..." autocomplete="off" onChange={handleChange}/>
           </div>
